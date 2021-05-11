835b210d5c1c8bc3e61d5629ffb58832f10272f8
prithvivenkatachalapathy <78224119+prithvivenkatachalapathy@users.noreply.github.com>
2021-05-11 17:42:21 -0400

    Updated app.py

--- app/app.py
+++ app/app.py
@@ -144,6 +144,88 @@ def api_delete(biostats_id) -> str:
     resp = Response(status=200, mimetype='application/json')
     return resp
 
+QUICK_MAIL_VERIFICATION_API_KEY = 'xxxx'
+SEND_GRID_API_KEY = 'xxxx'
+
+
+@app.route("/", methods=['GET'])
+def home():
+    return render_template("home.html")
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+        ##to get the registration data
+        fname = request.form.get("firstname")
+        lname = request.form.get("lastname")
+        address = request.form.get("address")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        ##to avoid duplicate emails registered
+        cursor = mysql.get_db().cursor()
+        cursor.execute('SELECT email FROM information where email=%s', email)
+        checkIfemailExists = cursor.fetchall()
+        if checkIfemailExists:
+            return render_template("register.html",
+                                   msg="Sorry, the email is already taken! Please use a different email")
+        ##to verify the email
+        client = quickemailverification.Client(QUICK_MAIL_VERIFICATION_API_KEY)
+        quickemailverification = client.quickemailverification()
+        response = quickemailverification.verify(email)
+        for i in response:
+            if i['result'] == 'valid':
+                inputData = (fname, lname, address, email, password)
+                sql_insert_query = """INSERT INTO information(firstName, lastName, address, email,password) VALUES (%s, %s,%s, %s,%s) """
+                cursor.execute(sql_insert_query, inputData)
+                mysql.get_db().commit()
+                return redirect("home.html", code=302)
+            else:
+                return render_template("register.html",
+                                       msg="Sorry, this is a invalid email, Please try with a valid mail")
+
+    if request.method == "GET":
+        return render_template("register.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        cursor = mysql.get_db().cursor()
+        cursor.execute('SELECT email,password FROM information where email=%s and password =%s', (email, password))
+        result = cursor.fetchall()
+        json_result = json.dumps(result)
+        print(result)
+        print(json_result)
+        if (result):
+            return render_template("homepageAfterLogin.html", msg="Logged In Successfully!!!")
+        else:
+            return render_template("login.html", msg="Incorret Login ID/Password !")
+
+    if request.method == "GET":
+        return render_template("login.html")
+
+@app.route("/sendMail", methods=['POST'])
+def sendmail():
+    ## first- get the data from the page
+    toemail = request.form.get("email")
+    sub = request.form.get("subject")
+    content = request.form.get("myTextArea")
+    message = Mail(
+        from_email='pv36@njit.edu.com',
+        to_emails=toemail,
+        subject=sub,
+        html_content=content)
+
+    try:
+        sg = SendGridAPIClient(SEND_GRID_API_KEY)
+        response = sg.send(message)
+    except Exception as e:
+        print(e.message)
+    return render_template("homepageAfterLogin.html", msg="Message Sent")
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0')
